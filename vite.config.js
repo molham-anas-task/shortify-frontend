c6a5737adc180398d6441a0ec71a5b73bb8030cc
Anas <molham.anas.task@gmail.com>
2025-05-05 18:08:02 +0300

    add allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 5173,
+    allowedHosts: ["shortify-frontend.onrender.com"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
