c9a84e7e02985296c0c3e297ad9ecfc2c7b07a2c
Anas <molham.anas.task@gmail.com>
2025-05-05 17:51:43 +0300

    add ErrorPage

--- src/pages/error-page.tsx
+++ src/pages/error-page.tsx
@@ -1,8 +1,10 @@
-export default function ErrorPage() {
+const ErrorPage = () => {
   return (
     <div className="text-center">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
     </div>
   );
-}
+};
+
+export default ErrorPage;
